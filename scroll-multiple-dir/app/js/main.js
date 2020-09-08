c88c0e5d0d0615ea804bf1a74f56f69dedd74826
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-08 20:10:10 +0200

    3steps built app

--- scroll-multiple-dir/app/js/main.js
+++ scroll-multiple-dir/app/js/main.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
 
 	// Init ScrollMagic
-	var controller = new ScrollMagic.Controller();
+	const controller = new ScrollMagic.Controller();
+
+	/*   Step   1  -  pin  the main section   */
+
+	/*   Step   2  -  Create a timeline playing during 300% of scrolling   */
+
+	/*   Step   3  -  Add a timeline to the scene   */
+
+
+	const horizontalMoveTl = new TimelineMax();
+	horizontalMoveTl
+		.to('.horizontal-container', 1, {x: '-66.666%', ease: Linear.easeNone});
+
+	const pinMainScene = new ScrollMagic.Scene({
+		triggerElement: '#main',
+		triggerHook: 0,
+		duration: '300%'
+	})
+	.setTween(horizontalMoveTl)
+	.setPin('#main')
+	.addTo(controller);
 
 });
 
