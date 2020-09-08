15035c7c53c2a3964b9fa730828abc26d78ff6f8
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-08 16:58:06 +0200

    added pin wrapper 01 - build secne01

--- cross-reveal-sections/01-Html-Css/app/assets/js/main.js
+++ cross-reveal-sections/01-Html-Css/app/assets/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+  // Init ScrollMagic
+  const controller = new ScrollMagic.Controller();
 
+
+  //  Scene 1 - pin the second section
+  const pinScene1 = new ScrollMagic.Scene({
+    triggerElement: '#slide01',
+    triggerHook: 0,
+    duration: '100%'
+  })
+  .setPin('#slide01 .pin-wrapper')
+  .addTo(controller)
+  ;
 
 });
