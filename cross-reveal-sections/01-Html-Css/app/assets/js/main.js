5d92e422fe069d97e25f4261446f62069f18de14
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-08 17:12:37 +0200

    setted the others scenes

--- cross-reveal-sections/01-Html-Css/app/assets/js/main.js
+++ cross-reveal-sections/01-Html-Css/app/assets/js/main.js
@@ -5,12 +5,42 @@ $(document).ready(function(){
 
 
   //  Scene 1 - pin the second section
-  const pinScene1 = new ScrollMagic.Scene({
+  const pinScene01 = new ScrollMagic.Scene({
     triggerElement: '#slide01',
     triggerHook: 0,
     duration: '100%'
   })
   .setPin('#slide01 .pin-wrapper')
+  .addTo(controller)
+  ;
+
+  //  Scene 2 - pin the third section
+  const pinScene02 = new ScrollMagic.Scene({
+    triggerElement: '#slide01',
+    triggerHook: 0,
+    duration: '200%'
+  })
+  .setPin('#slide02 .pin-wrapper')
+  .addTo(controller)
+  ;
+
+   //  Scene 3 - pin the fourth section
+   const pinScene03 = new ScrollMagic.Scene({
+    triggerElement: '#slide02',
+    triggerHook: 0,
+    duration: '300%'
+  })
+  .setPin('#slide03 .pin-wrapper')
+  .addTo(controller)
+  ;
+
+   //  Scene 3 - pin the fifth section
+   const pinScene04 = new ScrollMagic.Scene({
+    triggerElement: '#slide03',
+    triggerHook: 0,
+    duration: '400%'
+  })
+  .setPin('#slide04 .pin-wrapper')
   .addTo(controller)
   ;
 
